981d38a47acdadc95b82774948e43f3a5043f532
Tim Crone <tjcrone@gmail.com>
2016-09-05 11:03:09 -0400

    Updates to sampleout.m

--- src/sampleout.m
+++ src/sampleout.m
@@ -3,12 +3,14 @@ function [tout, Tout, crackedout, qzout] = sampleout()
 %
 % Timothy Crone
 
-files = {'../in_out/k215e15_stead02_out' ...
-  '../in_out/k215e15_crack01_out' ...
-  '../in_out/k215e15_crack02_out' ...
-  '../in_out/k215e15_crack03_out' ...
-  '../in_out/k215e15_crack04_out' ...
-  '../in_out/k215e15_crack05_out'};
+perm = '215e16';
+
+files = {sprintf('../in_out/k%s_stead02_out', perm) ...
+  sprintf('../in_out/k%s_uncrack01_out', perm) ...
+  sprintf('../in_out/k%s_uncrack02_out', perm) ...
+  sprintf('../in_out/k%s_uncrack03_out', perm) ...
+  sprintf('../in_out/k%s_uncrack04_out', perm)};% ...
+  %sprintf('../in_out/k%s_crack05_out', perm)};
 
 % load first file
 load(files{1});
@@ -26,7 +28,7 @@ load(files{2});
 Tall = ones(nz2,nx2,nt2+nt1-1)*Tout(end,1,1);
 Tall(1:nz1,1:nx1,1:nt1) = Tout1;
 Tall(:,:,nt1+1:end) = Tout(:,:,2:end);
-crackedall = Tall*0;
+crackedall = logical(Tall*0);
 crackedall(1:nz1,1:nx1,1:nt1) = crackedout1;
 crackedall(:,:,nt1+1:end) = crackedout(:,:,2:end);
 qzall = zeros(nz2+1,nx2,nt2+nt1-1);
@@ -43,13 +45,28 @@ for i = 3:length(files)
   tall = [tall tout(2:end)+tall(end)];
 end
 
-% interpolate
+% clear some variables to free up memory
+clear Pout Tout Tout1 cfout crackedout crackedout1 qxout qzout qzout1 rhofout
+
+% define interpolation times
 tinterval = 100; % years
-tout = 0:tinterval*365*24*60*60:tall(end);
+tmax = tall(end);
+%if tmax/60/60/24/365/1000>325
+%  tmax = 325*1000*365*24*60*60;
+%end
+tout = 0:tinterval*365*24*60*60:tmax;
+
+% interpolate Tout
 [X,Y,Z] = meshgrid(1:nx2,1:nz2,tall);
 [X2,Y2,Z2] = meshgrid(1:nx2,1:nz2,tout);
 Tout = interp3(X,Y,Z,Tall,X2,Y2,Z2);
-crackedout= interp3(X,Y,Z,crackedall,X2,Y2,Z2);
+clear Tall;
+
+% interpolate crackedout
+crackedout= interp3(X,Y,Z,crackedall,X2,Y2,Z2, 'nearest');
+clear crackedall;
+
+% interpolate qzout
 [X,Y,Z] = meshgrid(1:nx2,1:nz2+1,tall);
 [X2,Y2,Z2] = meshgrid(1:nx2,1:nz2+1,tout);
 qzout= interp3(X,Y,Z,qzall,X2,Y2,Z2);
