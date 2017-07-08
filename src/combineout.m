658f48485ab8ba15660b97980db398f82624b006
Timothy Crone <tjcrone@gmail.com>
2017-07-08 14:14:13 -0400

    Add outputs to combineout.m

--- src/combineout.m
+++ src/combineout.m
@@ -1,4 +1,4 @@
-function [Tout] = combineout(basename)
+function [Tout, rhofout, cfout, qzout, crackedout, tout] = combineout(basename)
 
 %basename = '/Users/tjc/research/crackingfronts/in_out/k215e15/k215e15_stead03*';
 
@@ -11,8 +11,18 @@ for file = files'
   load(filename);
   if i==1
     Tout = T2;
+    rhofout = rhof2;
+    cfout = cf2;
+    qzout = qz2;
+    crackedout = cracked;
+    tout = t;
   else
     Tout(:,:,i) = T2;
+    rhofout(:,:,i) = rhof2;
+    cfout(:,:,i) = cf2;
+    qzout(:,:,i) = qz2;
+    crackedout(:,:,i) = cracked;
+    tout(:,:,i) = t;
   end
   i = i+1;
 end
