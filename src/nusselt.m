1fbf168a480b3ed22dc68fc6402c1943146aa9e2
Timothy Crone <tjcrone@gmail.com>
2017-07-14 16:34:27 -0400

    Fix typo in nusselt.m

--- src/nusselt.m
+++ src/nusselt.m
@@ -8,30 +8,33 @@ function [Nu] = nusselt(nx,nz,d,T,P,lamdam,Tbt,Tbb,kx,kz,g, ...
 % Nusselt numbers are computed only for the boundaries that
 % separate horizontal control volume bands in the model domain.
 
-% calculate rhof,cf,qx,qz
+% load thermo tables
 global TT PP RHO CP
 if isempty(TT)
    load('../hydrotables/hydrotab8.mat');
 end
 
+% fluid properties
 rhof = interptim(PP,TT,RHO,P./100000,T);
 cf = interptim(PP,TT,CP,P./100000,T);
 mu = dynvisc(T);
 [qx,qz] = darcy(nx,nz,P,rhof,rhobb,kx,kz,mu,g,d,Pbt,Pbb,Pbr,Pbl,T);
 
-% compute conductive transport
+% conductive transport
 Qc = -lamdam.*(Tbb(1,1)-Tbt(1,1)).*((nx+1)/(nz+1));
 
-% calculate the conductive heat transport across each boundary (with convection)***
+% conductive heat transport across each boundary (with convection)
 Qcc = -lamdam.*(T(2:end,:)-T(1:end-1,:));
 
-% ***Calculate the Advective Heat Transport***
-Qan = T(2:end,:)  .*rhof(2:end,:)  .* cf(2:end,:)  .*qz(2:end-1,:).*(qz(2:end-1,:)<0);
-Qas = T(1:end-1,:).*rhof(1:end-1,:).* cf(2:end,:)  .*qz(2:end-1,:).*(qz(2:end-1,:)>=0);
+% advective heat transport
+Qan = T(2:end,:) .* rhof(2:end,:) .* cf(2:end,:) .* qz(2:end-1,:) .* ...
+  double(qz(2:end-1,:)<0);
+Qas = T(1:end-1,:) .* rhof(1:end-1,:) .* cf(1:end-1:end,:) .* qz(2:end-1,:) .* ...
+  double(qz(2:end-1,:)>=0);
 Qa = (Qan+Qas)*d;
 
-%***Calculate Total Q***
+% total Q
 Q = sum(Qcc+Qa,2);
 
-%***Calculate the Nusselt Number (for each band of horizontal boxes)
+% Nusselt number (for each band of horizontal boxes)
 Nu = Q./Qc;
