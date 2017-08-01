a2d536c47f7dc981f2ddee3a814e67b19014fd8f
J.-A. Olive <jaolive@ldeo.columbia.edu>
2017-08-01 07:39:35 -0700

    Add fault_permeability.m

---
+++ src/fault_permeability.m
@@ -0,0 +1,20 @@
+function [ kx, kz] = fault_permeability(input)
+% [ kx, kz] = fault_permeability(input)
+%   Calculates the permeability field including a fault
+%   of permeability k_fault surrounded by background permeability k_bg
+%   fault crosses top of model at fault_xs, has dip fault_dip (degrees)
+%   and width fault_width, extends down to max depth of fault_depth
+
+% initialize permeability array with background permeability
+input.kx = ones(input.nz, input.nx)*input.k_bg;
+input.kz = ones(input.nz, input.nx)*input.k_bg;
+
+% construct an array of "distance to the fault line"
+DF = abs( tand(input.fault_dip).*input.X  + input.Z - tand(input.fault_dip)*fault_xs) ./ sqrt(1+tand(input.fault_dip).^2);
+
+kx(DF<=input.fault_width/2 & input.Z<=input.fault_depth) = input.k_fault;
+kz(DF<=input.fault_width/2 & input.Z<=input.fault_depth) = input.k_fault;
+
+
+end
+
