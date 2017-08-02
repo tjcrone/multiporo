8c50b030b874d2041242e0dc679d85fa403be5b3
Tim Crone <tjcrone@gmail.com>
2017-08-02 10:31:39 -0400

    Allow anisotropic permeability

--- src/fault_permeability.m
+++ src/fault_permeability.m
@@ -6,12 +6,13 @@ function [ kx, kz] = fault_permeability(input)
 %   and width fault_width, extends down to max depth of fault_depth
 
 % initialize permeability array with background permeability
-kx = ones(input.nz, input.nx)*input.k_bg;
-kz = ones(input.nz, input.nx)*input.k_bg;
+kx = ones(input.nz, input.nx)*input.kx;
+kz = ones(input.nz, input.nx)*input.kz;
 
 % construct an array of "distance to the fault line"
 fault_distance = abs( tand(input.fault_dip).*input.X  + input.Z - ...
   tand(input.fault_dip)*input.fault_xs) ./ sqrt(1+tand(input.fault_dip).^2);
 
-kx(fault_distance<=input.fault_width/2 & input.Z<=input.fault_depth) = input.k_fault;
-kz(fault_distance<=input.fault_width/2 & input.Z<=input.fault_depth) = input.k_fault;
+% set permeability wthiin fault
+kx(fault_distance<=input.fault_width/2 & input.Z<=input.fault_depth) = input.kx_fault;
+kz(fault_distance<=input.fault_width/2 & input.Z<=input.fault_depth) = input.kz_fault;
